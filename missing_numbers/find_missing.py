c7f9904c107cd053104360962b5486b53f4af924
NyondoPaul <nyondopaul23@gmail.com>
2017-07-05 19:50:16 +0300

    add checks for empty and same entries

--- missing_numbers/find_missing.py
+++ missing_numbers/find_missing.py
@@ -1,9 +1,15 @@
 """ compares both lists and returns missing value """
 def find_missing(array_1, array_2):
     """ compares both lists and returns missing value """
-    if len(array_1) > len(array_2):
+    if len(array_1) == 0 and len(array_2) == 0:
+        return 0
+    elif len(array_1) > len(array_2):
         missing = list(set(array_1) - set(array_2))
         return int(missing[0])
-    missing = list(set(array_2) - set(array_1))
-    return int(missing[0])
+    elif len(array_1) < len(array_2):
+        missing = list(set(array_2) - set(array_1))
+        return int(missing[0])
+    elif len(array_1) == len(array_2):
+        return [ 0 for num in array_1 ]
+
 
