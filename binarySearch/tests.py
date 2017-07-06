65cef17ff0241999e7e253f9e32a2408bae12d21
NyondoPaul <nyondopaul23@gmail.com>
2017-07-06 14:54:16 +0300

    Add search not found, small, large lists

--- binarySearch/tests.py
+++ binarySearch/tests.py
@@ -6,10 +6,28 @@ class TestBinary(unittest.TestCase):
     """ unit tests for the BinarySearch Class"""
     def setUp(self):
         """ configuration before each test run"""
-        self.search = BinarySearch(6, 2)
+        self.b_search = BinarySearch(20, 1)
     def test_init(self):
         """ test the init method for BinarySearch"""
-        self.assertEqual([0, 2, 4, 6, 8, 10], self.search.array)
+        self.assertEqual([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], self.b_search)
+    def test_search(self):
+        """ tests the binary search method """
+        self.assertDictEqual({'count': 3, 'index':1},self.b_search.search(1))
 
-if __name__ ==  "__main__":
+    def test_search2(self):
+        """ tests the binary search method small list """
+        self.b_search = BinarySearch(8, 2)
+        self.assertDictEqual({'count': 1, 'index':3},self.b_search.search(6))
+
+    def test_search3(self):
+        """ tests the binary search method small list """
+        self.b_search = BinarySearch(100, 5)
+        self.assertDictEqual({'count': 4, 'index':4},self.b_search.search(20))
+
+    def test_search_not_found(self):
+        """ tests the binary search method small list """
+        self.b_search = BinarySearch(100, 5)
+        self.assertDictEqual({'count': 5, 'index':-1},self.b_search.search(16))
+
+if __name__ == "__main__":
     unittest.main()
